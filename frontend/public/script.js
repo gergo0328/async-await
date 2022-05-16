9b4d9e76a87ac088a07415886eaf9628290d249d
gergo0328 <gergo0328@gmail.com>
2022-05-16 13:28:02 +0200

    fetch API

---
+++ frontend/public/script.js
@@ -0,0 +1,26 @@
+const loadEvent = async function(){
+    const rootElement = document.getElementById("root")
+
+   /* fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(function (response){
+        console.log(response);
+        return response.json()
+    
+    })
+    .then(function (json){
+        console.log(json.date);
+        rootElement.insertAdjacentHTML("beforeend", `
+            <img src="${json.hdurl}">
+        `)
+
+    })*/
+
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    const responseJson = await response.json()
+    rootElement.insertAdjacentHTML("beforeend", `
+            <img src="${responseJson.hdurl}">
+        `)
+
+}
+
+window.addEventListener("load", loadEvent)
